20d647c89677b79067acb52fff560cdb3c521ec4
MiguelTomaz <migueltomaz.c@gmail.com>
2024-04-12 22:09:16 +0100

    [API][Add] add route and poi from tourist

--- src/routes/route.js
+++ src/routes/route.js
@@ -23,6 +23,47 @@ router.route('/all').get(async (req, res) => {
     }
 });
 
+router.route('/add').post(async (req, res) => {
+    try {
+        const { name, city, category, created } = req.body;
+        if (!name || !city || !category || !created) {
+            return res.status(400).json({ error: 'Parâmetros ausentes. Certifique-se de fornecer name, city, category e created.' });
+        }
+        
+        const { success, message, routeId } = await routeController.addRouteFromTourist(name, city, category, created);
+
+        if (success) {
+            res.status(200).json({ success: true, message: message, routeId: routeId });
+        } else {
+            res.status(500).json({ error: 'Falha ao adicionar rota.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Falha ao adicionar rota2.' });
+    }
+});
+
+router.route('/addPoiToRoute').post(async (req, res) => {
+    try {
+        const { route_id, order_in_route, id} = req.body;
+        if (!route_id || !id) {
+            return res.status(400).json({ error: 'Parâmetros ausentes. Certifique-se de fornecer route_id e id.' });
+        }
+        
+        const { success, message, newPOIId } = await routeController.addPoiToRoute(route_id, order_in_route, id);
+        
+        if (success) {
+            res.status(200).json({ success: true, message: message, newPOIId: newPOIId });
+        } else {
+            res.status(500).json({ error: 'Falha ao adicionar POI à rota.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Falha ao adicionar POI à rota.' });
+    }
+});
+
+
 router.route('/details/:id').get(async (req, res) => {
     const routeId = req.params.id;
 
